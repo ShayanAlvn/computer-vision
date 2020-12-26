a6b8d7f3e951b82ed7e0d3575dbb2c3646118e81
shayan <shayan13771717@gmail.com>
2020-12-26 12:44:43 +0330

    neuralnetwork

---
+++ pyimagesearch/nn/neuralnetwork.py
@@ -0,0 +1,42 @@
+#import the necessary packages
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(self, layers, alpha=0.1):
+        # initialize the list of weights matrices, then store the
+        # network architecture and learning rate
+        self.W = []
+        self.layers = layers
+        self.alpha = alpha
+       
+        #start looping from the index of the first layer but
+        # stop before we reach the last two layers
+        for i in np.arange(0, len(layers) - 2):
+            # randomly initialize a weight matrix connecting the
+            # number of nodes in each respective layer together,
+            # adding an extra node for the bias
+            w = np.random.rand(layers[i] + 1 , layers[i+1] + 1)
+            self.W.append(w / np.sqrt(layers[i]))
+            
+        # the last two layers are a special case where the input
+        # connections need a bias term but the output does not
+        w = np.random.randn(layers[-2] + 1, layers[-1])
+        self.W.append(w / np.sqrt(layers[-2]))
+    
+    def __repr__(self):
+        # construct and return a string that represents the network
+        # architecture 
+        return "NeuralNetwork: {}".format(
+            "-".join(str(l) for l in self.layers))
+        
+    def sigmoid(self,x):
+        # compute and return the sigmoid activation value for a
+        # given input value
+        return 1.0 / (1 + np.exp(-x))
+    
+    def sigmoid_deriv(self , x):
+        # compute the derivative of the sigmoid function ASSUMING
+        # that ‘x‘ has already been passed through the ‘sigmoid‘
+        # function
+        return x * (1 - x)
+        
