279a4cfd4713e7bd407e706867352d27668e2409
shayan <shayan13771717@gmail.com>
2020-12-29 12:50:26 +0330

    keras_mnist

---
+++ pyimagesearch/nn/keras_mnist.py
@@ -0,0 +1,29 @@
+#import the necessary packages
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import SGD
+from sklearn import datasets
+import matplotlib.pyplot as plt 
+import numpy as np
+import argparse
+
+#construct the argument pars and pars the argument 
+ap = argparse.ArgumentParser()
+ap.add_argument("-o" , "--output", required=True,
+    help="path to the output loss/accuracy plot")
+args = vars(ap.parse_args())
+
+# grab the MNIST dataset (if this is your first time running this
+# script, the download may take a minute -- the 55MB MNIST dataset
+# will be downloaded)
+print("[INFO] loading MNIST (full) dataset...")
+dataset = datasets.fetch_mldata("MNIST Original")
+
+# scale the raw pixel intensities to the range [0, 1.0], then
+# construct the training and testing splits
+data = dataset.data.astype("float") / 255.0
+(trainX, testX, trainY, testY) = train_test_split(data,
+    dataset.target, test_size=0.25)
