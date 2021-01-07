b3f5c2270c17864024ccaba7f66f21561ac79585
shayan <shayan13771717@gmail.com>
2021-01-07 11:22:58 +0330

    shallownet for animals

---
+++ pyimagesearch/nn/conv/shallownet_animals.py
@@ -0,0 +1,7 @@
+#import the necessary packes
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from pyimagesearch.pre_processing import ImageToArrayPreprocessor 
+from pyimagesearch.pre_processing import SimplePreprocessor 
+from pyimagesearch.datasets import 
