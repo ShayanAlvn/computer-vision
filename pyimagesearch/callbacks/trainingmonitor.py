0f9b7667176783d1df64d6bc9c3bf84f5cf2728b
shayan <shayan13771717@gmail.com>
2021-02-07 15:19:52 +0330

    monitor training

--- pyimagesearch/callbacks/trainingmonitor.py
+++ pyimagesearch/callbacks/trainingmonitor.py
@@ -1,1 +1,27 @@
 #import the necessary packages
+from keras.callbacks import BaseLogger
+import matplotlib.pyplot as plt 
+import numpy as np 
+import json
+import os
+
+class TrainigMonitor(BaseLogger):
+    def __init__(self, figPath, jsonPath=None, startAt=0):
+        #store the output path for the figure, the path to the JSON
+        #serialized file, and the starting epoch
+        super(TrainigMonitor, self).__init__()
+        self.figPath = figPath
+        self.jsonPath = jsonPath
+        self.startAt = startAt
+
+    def on_train_begin(self, logs={}):
+        #initialize the history dictionary
+        self.H = {}
+
+        #if the JSON history path exists, load the training history
+        if self.jsonPath is not None:
+            if os.path.exists(self.jsonPath):
+                self.H = json.loads(open(self.jsonPath).read())
+
+                #check to see if a starting epoch was supplied
+                
